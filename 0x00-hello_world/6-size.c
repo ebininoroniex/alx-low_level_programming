8664bcc93ab33f5c03ac7d55dfef2ef7555f36e4
ebininoroniex <ebininoroniex@gmail.com>
2022-10-14 13:27:58 -0700

    C Programmingexit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,12 +17,12 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte\n", sizeof(char));
-	printf("Size of an int: %d bytes\n", sizeof(int));
-	printf("Size of a long int: %d bytes\n", sizeof(long int));
-	printf("Size of a long long int: %d bytes\n", sizeof(long long int));
-	printf("Size of a float: %d bytes\n", sizeof(float));
-	printf("Size of a double: %d bytes\n", sizeof(double));
-	printf("Size of a long double: %d bytes\n", sizeof(long double));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	printf("Size of a double: %d bytes(s)\n", sizeof(double));
+	printf("Size of a long double: %d bytes(s)\n", sizeof(long double));
 	return (0);
 }
