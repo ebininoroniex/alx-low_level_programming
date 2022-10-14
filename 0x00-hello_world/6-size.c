77cc91735f29851b7bfb5b08cd84ecf980f28579
ebininoroniex <ebininoroniex@gmail.com>
2022-10-14 13:37:43 -0700

    C Programmingexit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 /*
- * Filename: 5-printf.c
- * C program to print -with proper grammar, but the outcome is a piece of art
+ * Filename: 6-size.c
+ * C program to print various sizes of datatypes 
+ * 		compiled and run on computersi
  * Author: Ebinino
  *
  * Size of a char: 1 byte
@@ -21,12 +22,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %dbyte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	printf("Size of a double: %d byte(s)\n", sizeof(double));
-	printf("Size of a long double: %d byte(s)\n", sizeof(long double));
 	return (0);
 }
