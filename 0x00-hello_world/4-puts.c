cd3925b3a6a798ea44efcc00791765808e978904
ebininoroniex <ebininoroniex@gmail.com>
2022-10-14 11:53:38 -0700

    Hello, World!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+/* C program to print -Programming is like building a multilingual puzzle*/
+#include<stdio.h>
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0)
+}
