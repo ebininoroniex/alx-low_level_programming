ad17b3583a5b4ac097bebeaad1dd56c2206ead58
ebininoroniex <ebininoroniex@gmail.com>
2022-10-14 12:08:27 -0700

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 /* C program to print -Programming is like building a multilingual puzzle*/
 #include<stdio.h>
-int main (void)
+
+/**
+ *Main function to print "Programming is like building a multilingual puzzle
+ *
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0)
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
