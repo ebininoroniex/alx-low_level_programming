4c5f8c1ab7b477cccfae77a65911ab2667efd49c
ebininoroniex <ebininoroniex@gmail.com>
2022-10-16 12:55:26 -0700

    Variables, if, else, whileexit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,8 @@
 /* more headers goes there */
 /**
  * main - program that prints all single digit numbers of base 10 starting
- * from 0, followed by a new line
+ * from 0, only using putchar and without char variables
+ * followed by a new line
  *
  * Return: Always 0
  */
@@ -12,8 +13,9 @@
 int main(void)
 {
 int num;
+
 for (num = 0; num < 10; num++)
-	printf("%d", num);
-printf("\n");
+	putchar("%d", num);
+putchar("\n");
 return (0);
 }
