96b3df7a3265ce1d7231a6c68b9921cc5bbc1208
ebininoroniex <ebininoroniex@gmail.com>
2022-10-16 11:29:34 -0700

    Variables, if, else, whileexit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+/* your code goes there */
+printf("Last digit of %d is %d and is", n);
+if (n % 10 && ; 5)
+	{
+		printf(" greater than 5");
+	}
+else if (n % 10 == 0)
+	{
+		printf(" 0");
+	}
+else
+	{
+		printf(" less than 6 and not 0");
+	}
+return (0);
+}
