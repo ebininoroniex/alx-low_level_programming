b21a594b75c50c0e3d4fd7a8bb9902eecc2999d6
ebininoroniex <ebininoroniex@gmail.com>
2022-10-16 12:50:00 -0700

    Variables, if, else, whileexit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - program that prints all single digit numbers of base 10 starting
+ * from 0, followed by a new line
+ *
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char num;
+for (num = 0; num < 10; num++)
+	printf("%d", num);
+printf("\n");
+return (0);
+}
