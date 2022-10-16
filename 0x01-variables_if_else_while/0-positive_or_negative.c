3a0f881bd111cbd6f18d849f69b5cf176084dc40
ebininoroniex <ebininoroniex@gmail.com>
2022-10-16 11:00:12 -0700

    Variables, if, else, whileexit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
-	printf("%d is positive");
+	printf("%d is positive\n", n);
 if (n == 0)
-	printf("%d is zero");
+	printf("%d is zero\n", n);
 else
-	printf("%d is negative");
+	printf("%d is negative\n", n);
 return (0);
 }
