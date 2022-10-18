898a4810350d0fdade8e84a2a54dd1c98a1ed3e1
ebininoroniex <ebininoroniex@gmail.com>
2022-10-18 13:30:05 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
-  * File: main.h
-  * Author: Ebinino
-  * Description: Header file for functions and nested loops
-  */
 int _putchar(char c);
-
-
-
-
-
-
-
-
-
-
 #endif
