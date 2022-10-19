25ed73984a5dde55da4e24692cd6df8f4e336f53
ebininoroniex <ebininoroniex@gmail.com>
2022-10-19 16:41:41 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: integer a
+ * @b: integer b
+ * Description: adds two integers a and b
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
