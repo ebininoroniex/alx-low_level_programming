5226244a8a82e1bd8106965e811e99e0a5ebc3fd
ebininoroniex <ebininoroniex@gmail.com>
2022-10-25 11:45:41 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character, followed by a new line
+ *@str: string input
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
