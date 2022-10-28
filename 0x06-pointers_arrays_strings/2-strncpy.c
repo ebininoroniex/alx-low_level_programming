b48510d3b7e92a4bfe943fcde8b558e524336f4a
ebininoroniex <ebininoroniex@gmail.com>
2022-10-28 11:52:05 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  **_strncpy - copy a string
  * @dest: destination string
  * @src: source string  * @n: amount of characters to copy
+ * @n: integer
  * Return: pointer to destination string
  */
 char *_strncpy(char *dest, char *src, int n)
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 15 if s1 is greater than s2, -15 if s2 is greater than s1, 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			return (15);
+		if (s1[i] < s2[i])
+			return (-15);
+	}
+	if (s1[i] > s2[i])
+		return (15);
+	if (s1[i] < s2[i])
+		return (-15);
+	return (0);
+}
+
