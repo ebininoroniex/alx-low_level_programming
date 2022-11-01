d18787cec2619ef311149ec8375a3dfdb3d4a796
ebininoroniex <ebininoroniex@gmail.com>
2022-11-01 12:01:59 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
