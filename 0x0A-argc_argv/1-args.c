65c00edb6fdad7e6b5a15bf6796786984813c621
ebininoroniex <ebininoroniex@gmail.com>
2022-11-08 11:38:17 -0800

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - progrram entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
