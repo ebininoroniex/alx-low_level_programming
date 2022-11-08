d87251e596b8eff9c65847ff266fc1d8c71dbde4
ebininoroniex <ebininoroniex@gmail.com>
2022-11-08 11:29:20 -0800

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - progrram entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
