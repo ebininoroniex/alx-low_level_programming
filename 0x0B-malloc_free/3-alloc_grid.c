b408a27c8bb37b356dc31c846e4bc749d3937fef
ebininoroniex <ebininoroniex@gmail.com>
2022-11-09 11:20:35 -0800

    argc, argv

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,29 +12,21 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+	int **grid, i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
-
-	grid[i] = (int **)malloc(sizeof(int *) * height);
-
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
-	for (i = 0; i < height; i++)
+	for (; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 			return (NULL);
 	}
 	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-		{
+		for (; j < width; j++)
 			grid[i][j] = 0;
-		}
-	}
 	return (grid);
 }
