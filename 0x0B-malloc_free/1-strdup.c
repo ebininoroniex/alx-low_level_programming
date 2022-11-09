4dce3226bf93743ab539b5269d03c7f80f14f48d
ebininoroniex <ebininoroniex@gmail.com>
2022-11-09 09:35:55 -0800

    Malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,17 +11,18 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int counter, len;
-	char *copy;
+	char *dupString;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	for (len = 0; str[len] != '\0'; len++)
-		copy = malloc(sizeof(char) * (len + 1));
-	if (copy == NULL)
+	for (index = 0; str[index]; index++)
+		len++;
+	dupString = malloc(sizeof(char) * (len + 1));
+	if (dupString == NULL)
 		return (NULL);
-	for (counter = 0; counter < len; counter++)
-		copy[counter] = str[counter];
-	copy[counter] = '\0';
-	return (copy);
+	for (index = 0; str[index]; index++)
+		dupString[index] = str[index];
+	dupString[len] = '\0';
+	return (dupString);
 }
