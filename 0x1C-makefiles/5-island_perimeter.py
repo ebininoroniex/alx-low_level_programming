dacf237e398714860fce6dcd83c82156086fb3b6
ebininoroniex <ebininoroniex@gmail.com>
2023-03-27 08:43:55 -0700

    Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python3
+"""
+module for perimeter of island
+"""
 
 
 def island_perimeter(grid):
-    """ Function that returns detail of a grid"""
-
-    if not isinstance(grid, list) or not len(grid):
-        return 0
-    y = len(grid) - 1
-    x = len(grid[0]) - 1
-
-    prim = 0
-
-    for h in range(y):
-        for r in range(x):
-            if grid[h][r] == 1:
-                prim += check_surround(grid, h, r)
-    return prim
-
-
-def check_surround(grid, row, idx):
-    """ Function that checks the surroundings of a given tile
+    """returns the perimeter of the island
+    Args:
+         grid (list) = list of integers
     """
+
+    width = len(grid[0])
     height = len(grid)
-    length = len(grid[0])
-    sur = [0, 0, 0, 0]
+    edges = 0
+    size = 0
 
-    if row - 1 >= 0:  # up
-        if not grid[row - 1][idx]:
-            sur[0] = 1
-
-    if row + 1 < height:  # down
-        if not grid[row + 1][idx]:
-            sur[1] = 1
-
-    if idx - 1 >= 0:  # left
-        if not grid[row][idx - 1]:
-            sur[2] = 1
-
-    if idx + 1 < length:  # right
-        if not grid[row][idx + 1]:
-            sur[3] = 1
-
-    return sum(sur)
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                # checking for boundery or a lake
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
